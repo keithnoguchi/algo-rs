730b5ea5ea6e4189ecaf043fef04549f6bcb0665
Keith Noguchi <keith@noguchi.us>
2023-03-20 14:39:19 -0700

    gcd: Greatest Common Divisor (#2)

---
+++ gcd/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    assert_eq!(gcd(36, 24), 12);
+}
+
+fn gcd(mut m: u64, mut n: u64) -> u64 {
+    assert!(m != 0 && n != 0);
+    while m != 0 {
+        if m < n {
+            std::mem::swap(&mut m, &mut n);
+        }
+        m %= n;
+    }
+    n
+}
