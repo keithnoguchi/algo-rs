4f72e35d73e4f435f2496002c281d0572e9c84db
Keith Noguchi <keith@noguchi.us>
2023-03-20 21:43:13 -0700

    bubble_sort: check if it's already sorted

--- bubble_sort/src/main.rs
+++ bubble_sort/src/main.rs
@@ -3,10 +3,15 @@
 /// O(n^2)
 pub fn bubble_sort<T: PartialOrd>(v: &mut [T]) {
     for i in 1..v.len() {
+        let mut sorted = true;
         for j in 0..v.len() - i {
             if v[j] > v[j + 1] {
                 v.swap(j, j + 1);
+                sorted = false;
             }
+        }
+        if sorted {
+            return;
         }
     }
 }
