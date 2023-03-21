4dcaf805711b5682c5ab8893d3757e4e3cfd505b
Keith Noguchi <keith@noguchi.us>
2023-03-20 21:50:53 -0700

    Add debug output

---
+++ README.md
@@ -0,0 +1,5 @@
+# Rust Algorithms
+
+## Sort
+
+- [Bubble sort](bubble_sort/src/main.rs)
--- bubble_sort/src/main.rs
+++ bubble_sort/src/main.rs
@@ -1,15 +1,19 @@
 //! A bubble sort.
 
+use std::fmt::Debug;
+
 /// O(n^2)
-pub fn bubble_sort<T: PartialOrd>(v: &mut [T]) {
+pub fn bubble_sort<T: Debug + PartialOrd>(v: &mut [T]) {
     for i in 1..v.len() {
         let mut sorted = true;
+        println!("{i:03}: {v:?}");
         for j in 0..v.len() - i {
             if v[j] > v[j + 1] {
                 v.swap(j, j + 1);
                 sorted = false;
             }
         }
+        println!("{i:03}: {v:?}");
         if sorted {
             return;
         }
@@ -17,7 +21,7 @@ pub fn bubble_sort<T: PartialOrd>(v: &mut [T]) {
 }
 
 fn main() {
-    let mut v = vec![4, 6, 1, 8, 11, 12];
+    let mut v = vec![4, 6, 12, 11, 8, 1];
     bubble_sort(&mut v);
     assert_eq!(v, vec![1, 4, 6, 8, 11, 12]);
 }
