bcd3697d7746223a6c1292cd0167e11ea9eb44ae
Keith Noguchi <keith@noguchi.us>
2023-03-22 16:13:57 -0700

    huffman: Create a HScore list

--- huffman/src/main.rs
+++ huffman/src/main.rs
@@ -1,3 +1,40 @@
+//! A Huffman Encoding
+
+#![forbid(missing_debug_implementations)]
+
+use std::collections::BTreeMap;
+
+#[derive(Debug)]
+pub struct HScore {
+    h: HuffmanNode,
+    score: i32,
+}
+
+#[derive(Debug)]
+pub enum HuffmanNode {
+    Tree(Box<Self>, Box<Self>),
+    Lead(char),
+}
+
+pub fn build_tree(s: &str) {
+    let mut map = BTreeMap::new();
+
+    for c in s.chars() {
+        let mut e = map.entry(c).or_insert(0);
+        *e += 1;
+    }
+    let mut tlist: Vec<HScore> = map
+        .into_iter()
+        .map(|(k, v)| {
+            HScore {
+                h: HuffmanNode::Lead(k),
+                score: v,
+            }
+        })
+        .collect();
+    println!("{tlist:#?}");
+}
+
 fn main() {
-    println!("Hello, world!");
+    build_tree("Hello, world!");
 }
