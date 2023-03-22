b3c853d3c559ca6e98c75f25053926913210945c
Keith Noguchi <keith@noguchi.us>
2023-03-22 10:01:16 -0700

    tree: A bare bone tree structure

--- tree/src/main.rs
+++ tree/src/main.rs
@@ -1,3 +1,26 @@
+//! A binary tree.
+
+#![forbid(missing_debug_implementations)]
+
+use std::fmt::Debug;
+
+#[derive(Debug)]
+pub struct Tree<T: Debug>(Option<Box<Node<T>>>);
+
+#[derive(Debug)]
+pub struct Node<T: Debug> {
+    data: T,
+    left: Tree<T>,
+    right: Tree<T>,
+}
+
+impl<T: Debug> Tree<T> {
+    pub const fn new() -> Self {
+        Self(None)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut tree = Tree::<String>::new();
+    println!("{tree:?}");
 }
