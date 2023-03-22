a1c5c2233bc4e7cec513ba572d60f74dd06f35f4
Keith Noguchi <keith@noguchi.us>
2023-03-21 18:40:37 -0700

    list: push_back function

--- list/src/main.rs
+++ list/src/main.rs
@@ -18,12 +18,21 @@ impl<T: Debug> LinkedList<T> {
         let next = self.0.take();
         self.0 = Some((data, Box::new(Self(next))));
     }
+
+    pub fn push_back(&mut self, data: T) {
+        match self.0 {
+            Some((_, ref mut next)) => next.push_back(data),
+            None => self.push_front(data),
+        }
+    }
 }
 
 fn main() {
     let mut list = LinkedList::<&str>::new();
-    list.push_front("first");
-    list.push_front("second");
-    list.push_front("third");
+    list.push_front("push front first");
+    list.push_front("push front second");
+    list.push_front("push front third");
+    list.push_back("push back first");
+    list.push_back("push back second");
     println!("{list:?}");
 }
