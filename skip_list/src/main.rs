2b3b83b0a36a38e52069d81c7f02632b890427a8
Keith Noguchi <keith@noguchi.us>
2023-03-22 14:28:27 -0700

    skip-list: Node<T> type

--- skip_list/src/main.rs
+++ skip_list/src/main.rs
@@ -1,3 +1,28 @@
+//! A skip list
+
+use std::cell::RefCell;
+use std::fmt::Debug;
+use std::rc::Rc;
+
+#[derive(Debug)]
+struct Node<T: Debug> {
+    data: Rc<RefCell<T>>,
+    next: Option<Rc<RefCell<Self>>>,
+    down: Option<Rc<RefCell<Self>>>,
+}
+
+impl<T: Debug> From<T> for Node<T> {
+    fn from(data: T) -> Self {
+        Self {
+            data: Rc::new(RefCell::new(data)),
+            next: None,
+            down: None,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let node = Node::from(1);
+
+    println!("{node:?}");
 }
