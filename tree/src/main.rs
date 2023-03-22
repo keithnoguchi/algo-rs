a319e3388ec2da250531eaf07c6c2724ec43fd03
Keith Noguchi <keith@noguchi.us>
2023-03-22 10:22:37 -0700

    tree: Default impl

--- tree/src/main.rs
+++ tree/src/main.rs
@@ -7,6 +7,12 @@ use std::fmt::Debug;
 #[derive(Debug)]
 pub struct Tree<T: Debug>(Option<Box<Node<T>>>);
 
+impl<T: Debug> Default for Tree<T> {
+    fn default() -> Self {
+        Self(None)
+    }
+}
+
 #[derive(Debug)]
 pub struct Node<T: Debug> {
     data: T,
@@ -14,13 +20,7 @@ pub struct Node<T: Debug> {
     right: Tree<T>,
 }
 
-impl<T: Debug> Tree<T> {
-    pub const fn new() -> Self {
-        Self(None)
-    }
-}
-
 fn main() {
-    let mut tree = Tree::<String>::new();
+    let tree = Tree::<String>::default();
     println!("{tree:?}");
 }
