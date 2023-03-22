65487153f96e8b008f1b0be28e7302e9475e3ed1
Keith Noguchi <keith@noguchi.us>
2023-03-21 18:05:41 -0700

    list: Default impl

--- list/src/main.rs
+++ list/src/main.rs
@@ -3,9 +3,15 @@ use std::fmt::Debug;
 #[derive(Debug)]
 pub struct LinkedList<T: Debug>(Option<(T, Box<Self>)>);
 
+impl<T: Debug> Default for LinkedList<T> {
+    fn default() -> Self {
+        Self(None)
+    }
+}
+
 impl<T: Debug> LinkedList<T> {
     pub fn new() -> Self {
-        Self(None)
+        Self::default()
     }
 }
 
