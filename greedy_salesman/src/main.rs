18e367880d16257215f785494c4eb4515dba5069
Keith Noguchi <keith@noguchi.us>
2023-03-23 08:13:18 -0700

    greedy_salesman: Graph, Vertex, and Egdge types

--- greedy_salesman/src/main.rs
+++ greedy_salesman/src/main.rs
@@ -1,3 +1,30 @@
+//! Greedy algorithm for the traveling sales person problem
+
+#![forbid(unsafe_code, missing_debug_implementations)]
+
+use std::collections::HashMap;
+use std::hash::Hash;
+
+#[derive(Debug)]
+pub struct Graph<K: Hash, V, E> {
+    vertices: HashMap<K, Vertex<K, V>>,
+    edges: HashMap<K, Edge<K, E>>,
+}
+
+#[derive(Debug)]
+pub struct Vertex<K, T> {
+    id: K,
+    data: T,
+}
+
+#[derive(Debug)]
+pub struct Edge<K, T> {
+    id: K,
+    from: K,
+    to: K,
+    data: T,
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello greedy salesman");
 }
