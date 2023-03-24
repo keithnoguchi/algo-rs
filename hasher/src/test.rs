75b2ae4929d15da8e178843cb98d5d126ec54b13
Keith Noguchi <keith@noguchi.us>
2023-03-24 11:43:24 -0700

    hasher: Unit tests

--- hasher/src/test.rs
+++ hasher/src/test.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use super::hash;
 
 #[test]
@@ -10,4 +12,21 @@ fn same_input_same_hash() {
 fn different_input_different_hash() {
     let h = hash(55, "cat");
     assert!(hash(55, "cats") != h);
+}
+
+#[test]
+fn different_hash_with_different_order() {
+    let h = hash(55, "abc");
+    assert!(hash(55, "cba") != h);
+}
+
+#[test]
+fn diffrent_hash_with_different_numbers() {
+    let mut result = HashSet::new();
+
+    for i in 0..10_000 {
+        // It returns true if there is no identical hash
+        // before.
+        assert!(result.insert(hash(55, i)));
+    }
 }
