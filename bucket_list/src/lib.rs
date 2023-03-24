79df890f7f0f1225581f011fef7dcce21547af76
Keith Noguchi <keith@noguchi.us>
2023-03-24 15:09:51 -0700

    bucket_list: A bucket list for HashMap

--- bucket_list/src/lib.rs
+++ bucket_list/src/lib.rs
@@ -1,14 +1,22 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+//! A bucket list
+
+#![forbid(unsafe_code, missing_debug_implementations)]
+
+use std::hash::Hash;
+
+#[derive(Debug)]
+pub struct BucketList<K, V> {
+    seed: u64,
+    len: usize,
+    buckets: Vec<Vec<(K, V)>>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl<K: Eq + Hash, V> Default for BucketList<K, V> {
+    fn default() -> Self {
+        Self {
+            seed: rand::random(),
+            len: 0,
+            buckets: vec![Vec::new()],
+        }
     }
 }
