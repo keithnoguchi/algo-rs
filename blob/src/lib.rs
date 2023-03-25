fb4164c30c1fd7c79bf07990b4d39d16fc127f67
Keith Noguchi <keith@noguchi.us>
2023-03-25 16:11:47 -0700

    Error type

--- blob/src/lib.rs
+++ blob/src/lib.rs
@@ -1,14 +1,10 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+//! A blob data structure.
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#![forbid(unsafe_code, missing_debug_implementations)]
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[derive(Debug)]
+pub enum Error {
+    NoRoom,
+    TooBig(u64),
+    NotFound,
 }
