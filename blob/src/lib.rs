a2785483359d61930bd3b033d10c1a48c8899436
Keith Noguchi <keith@noguchi.us>
2023-03-25 16:16:39 -0700

    Trying thiserror...

--- blob/src/lib.rs
+++ blob/src/lib.rs
@@ -2,9 +2,16 @@
 
 #![forbid(unsafe_code, missing_debug_implementations)]
 
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
 pub enum Error {
+    #[error("No Room")]
     NoRoom,
+    #[error("Too Big")]
     TooBig(u64),
+    #[error("Not Found")]
     NotFound,
+    #[error("Bincode Error")]
+    BinCode(bincode::Error),
+    #[error("IO Error")]
+    Io(std::io::Error),
 }
