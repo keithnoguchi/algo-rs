6cc67fd8d2c87271989a6acc5bbade79f375646f
Keith Noguchi <keith@noguchi.us>
2023-03-25 11:47:22 -0700

    id_generator: GenData, EntiryActive, and GenManager types

--- id_generator/src/main.rs
+++ id_generator/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+//! An ID generator.
+
+#![forbid(unsafe_code, missing_debug_implementations)]
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct GenData {
+    pos: usize,
+    gen: u64,
 }
+
+#[derive(Debug)]
+pub struct EntityActive {
+    active: bool,
+    gen: u64,
+}
+
+#[derive(Debug)]
+pub struct GenManager {
+    items: Vec<EntityActive>,
+    drops: Vec<usize>,
+}
+
+impl Default for GenManager {
+    fn default() -> Self {
+        Self {
+            items: vec![],
+            drops: vec![],
+        }
+    }
+}
+
+fn main() {}
