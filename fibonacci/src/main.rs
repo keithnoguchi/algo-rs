b6ab40666fe050407febcb3822e571ccad1e4c82
Keith Noguchi <keith@noguchi.us>
2023-03-26 13:35:41 -0700

    fibonacci: Cleanup the code for readability

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,53 +1,25 @@
-//! Fibonacci sequences.
+//! A fibonacci sequence.
 
-/// O(n)
-fn fibonacci(n: i32) -> i32 {
+#![forbid(unsafe_code, missing_debug_implementations)]
+
+pub fn fibonacci(n: usize) -> usize {
     let mut a = 1;
     let mut b = 1;
-    let mut res = 1;
+
+    let mut result = a;
     for _ in 1..n {
-        res = a + b;
+        result = a + b;
         a = b;
-        b = res;
+        b = result;
     }
-    res
+    result
 }
 
-/// O(2n)
-fn fibonacci_recursive(n: i32) -> i32 {
-    if n <= 1 {
-        return 1;
-    }
-    fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
-}
-
-/// O(n)
-fn fibonacci_dynamic(n: i32) -> (i32, i32) {
-    if n < 1 {
-        return (1, 0);
-    }
-    let (result, previous) = fibonacci_dynamic(n - 1);
-    (result + previous, result)
-}
-
-#[test]
-fn test_fibonacci() {
-    for n in 0..=30 {
-        assert_eq!(fibonacci(n), fibonacci_recursive(n));
-    }
-}
-
-#[test]
-fn test_fibonacci_dynamic() {
-    for n in 0..=30 {
-        assert_eq!(fibonacci_dynamic(n).0, fibonacci(n));
-    }
-}
+#[cfg(test)]
+mod test;
 
 fn main() {
-    for n in 1..=20 {
+    for n in 0..=92 {
         println!("fibonacci({n}) = {}", fibonacci(n));
-        assert_eq!(fibonacci(n), fibonacci_recursive(n));
-        assert_eq!(fibonacci(n), fibonacci_dynamic(n).0);
     }
 }
