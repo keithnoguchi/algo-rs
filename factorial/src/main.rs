435c207ea5a4fe53681489305ea31ea0f5bd0a93
Keith Noguchi <keith@noguchi.us>
2023-03-26 14:52:03 -0700

    Factorial (#32)
    * factorial: A factorial sequence generator
    
    * factorial: A factorial sequence generator
    
    Both the iterative and recursive approach.

---
+++ factorial/src/main.rs
@@ -0,0 +1,20 @@
+//! A factorial sequence generator
+
+#![forbid(unsafe_code, missing_debug_implementations)]
+
+pub fn factorial(n: usize) -> usize {
+    let mut result = 1;
+    for x in 2..=n {
+        result *= x;
+    }
+    result
+}
+
+#[cfg(test)]
+mod test;
+
+fn main() {
+    for n in 0..=20 {
+        println!("factorial({n}) = {}", factorial(n));
+    }
+}
